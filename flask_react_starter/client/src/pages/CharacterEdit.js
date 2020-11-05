bc34aa218ce1e8a4ac28f8f3b3e37be058e8f5f5
Daniel Wilkins <11danielw@gmail.com>
2020-11-04 23:43:42 -0500

    Add placeholder values to character edit page

--- flask_react_starter/client/src/pages/CharacterEdit.js
+++ flask_react_starter/client/src/pages/CharacterEdit.js
@@ -1,42 +1,47 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styles from '../CSS_MODULES/character_form.module.css'
-import {useDispatch, useSelector} from 'react-redux'
-import {updateCharacter} from '../store/characters'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateCharacter } from '../store/characters'
 import { withRouter } from 'react-router-dom';
 
 
-function CharacterEdit({history}) {
-
-    const [name, setName] = useState("");
-    const [strength, setStrength] = useState(null);
-    const [dexterity, setDexterity] = useState(null);
-    const [constitution, setConstitution] = useState(null);
-    const [intelligence, setIntelligence] = useState(null);
-    const [wisdom, setWisdom] = useState(null);
-    const [charisma, setCharisma] = useState(null);
-    const [armorClass, setArmorClass] = useState(null);
-    const [maxHitpoints, setMaxHitpoints] = useState(null);
+function CharacterEdit({ history }) {
+    const selectedCharacterId = useSelector(state => state.ui.selectedCharacterId)
+    const selectedCharacter = useSelector(state => state.entities.characters[selectedCharacterId])
+    const [name, setname,] = useState("");
+    const [strength, setStrength] = useState("");
+    const [dexterity, setDexterity] = useState("");
+    const [constitution, setConstitution] = useState("");
+    const [intelligence, setIntelligence] = useState("");
+    const [wisdom, setWisdom] = useState("");
+    const [charisma, setCharisma] = useState("");
+    const [armorClass, setArmorClass] = useState("");
+    const [maxHitpoints, setMaxHitpoints] = useState("");
     const [features, setFeatures] = useState("");
     const [actions, setActions] = useState("");
     const userId = useSelector(state => state.session.userId)
     const dispatch = useDispatch()
 
+    const selectedCharacterId = useSelector(state => state.ui.selectedCharacterId)
+
+
     const handleClick = async (e) => {
         e.preventDefault()
 
         const res = await dispatch(updateCharacter(
-                                name,
-                                strength,
-                                dexterity,
-                                constitution,
-                                intelligence,
-                                wisdom,
-                                charisma,
-                                armorClass,
-                                maxHitpoints,
-                                features,
-                                actions,
-                                userId));
+            selectedCharacterId,
+            name,
+            strength,
+            dexterity,
+            constitution,
+            intelligence,
+            wisdom,
+            charisma,
+            armorClass,
+            maxHitpoints,
+            features,
+            actions,
+            userId));
 
         if (res.ok) {
             history.push('/create-character')
@@ -55,19 +60,19 @@ function CharacterEdit({history}) {
                 <h1>Create a Character</h1>
                 <hr className={styles.hr}></hr>
                 <h5 className={styles.field_title}>CHARACTER NAME</h5>
-                <input onChange={(e)=>setName(e.target.value)} className={styles.form_title_text} type="text" placeholder="Enter a name" />
+                <input onChange={(e) => setName(e.target.value)} className={styles.form_title_text} type="text" placeholder="Enter a name" />
                 <h5 className={styles.field_title}>ABILITY SCORES</h5>
                 <div className={styles.score_container}>
-                    <span>Strength Score</span><input type="number" min="0" max="20" onChange={(e)=>setStrength(e.target.value)}/>
-                    <span>Dexterity Score</span><input type="number" min="0" max="20" onChange={(e)=>setDexterity(e.target.value)}/>
-                    <span>Constitution Score</span><input type="number" min="0" max="20" onChange={(e)=>setConstitution(e.target.value)}/>
-                    <span>Intelligence Score</span><input type="number" min="0" max="20" onChange={(e)=>setIntelligence(e.target.value)}/>
-                    <span>Wisdom Score</span><input type="number" min="0" max="20" onChange={(e)=>setWisdom(e.target.value)}/>
-                    <span>Charisma Score</span><input type="number" min="0" max="20" onChange={(e)=>setCharisma(e.target.value)}/>
+                    <span>Strength Score</span><input type="number" min="0"     placeholder={strength} max="20" onChange={(e) => setStrength(e.target.value)} />
+                    <span>Dexterity Score</span><input type="number" min="0"    placeholder={dexterity} max="20" onChange={(e) => setDexterity(e.target.value)} />
+                    <span>Constitution Score</span><input type="number" min="0" placeholder={constitution} max="20" onChange={(e) => setConstitution(e.target.value)} />
+                    <span>Intelligence Score</span><input type="number" min="0" placeholder={intelligence} max="20" onChange={(e) => setIntelligence(e.target.value)} />
+                    <span>Wisdom Score</span><input type="number" min="0"       placeholder={wisdom} max="20" onChange={(e) => setWisdom(e.target.value)} />
+                    <span>Charisma Score</span><input type="number" min="0"     placeholder={charisma} max="20" onChange={(e) => setCharisma(e.target.value)} />
                 </div>
                 <h5>CHARACTER STATISTICS</h5>
-                <span>Armor Class</span><input type="number" min="0" max="20" onChange={(e)=>setArmorClass(e.target.value)}/>
-                <span>Maximum Hitpoints</span><input type="number" min="0" max="20" onChange={(e)=>setMaxHitpoints(e.target.value)}/>
+                <span>Armor Class</span><input type="number" min="0" max="20" onChange={(e) => setArmorClass(e.target.value)} />
+                <span>Maximum Hitpoints</span><input type="number" min="0" max="20" onChange={(e) => setMaxHitpoints(e.target.value)} />
                 <button className={styles.form_button} onClick={handleClick}>Add Character</button>
                 <button className={styles.form_button} onClick={handleCancel}>Cancel</button>
             </div>
